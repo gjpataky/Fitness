b47198f260b18081c37678019b534967812f9739
gjpataky <gjpataky@yahoo.com>
2020-02-04 21:58:02 -0600

    heroku

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useNewUrlParser: true
 });
 app.use(require("./routes/api.js"));
-app.use(require("./routes/view.js"));
+app.use(require("./routes/html.js"));
 // Start the server
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
